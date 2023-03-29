e17a6a0e7ccada1bb830a232c1443dba1b971876
VYankov7 <vyankov02@gmail.com>
2023-03-29 14:11:24 +0300

    update

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -32,10 +32,11 @@ const Intro = () => {
           className="app__video-overlay_circle flex__center"
           onClick={handleVideo}
         >
-          {playVideo 
-              ? <BsPauseFill color="#fff" fontSize={30} />
-              : <BsFillPlayFill color="#fff" fontSize={30} />
-          }
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
         </div>
       </div>
     </div>
